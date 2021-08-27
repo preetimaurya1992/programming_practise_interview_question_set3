05b4827188e169c9c2194fc1198d440e40fc298d
Preeti Maurya <preetimaurya3344@gmail.com>
2021-08-27 16:31:39 +0530

    added string palindrome programm

---
+++ string_palindrome.cpp
@@ -0,0 +1,20 @@
+//10.Write a Program to check if String is a palindrome or not
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s,rev;
+    cout<<"enter the string"<<endl;
+    cin>>s;
+    string original=s;
+    int n=s.length();
+    
+    for(int i=n-1;i<=0;i--){
+         //rev[i]=s[i];
+         cout<<s[i]<<endl;
+    }
+       if(s==original)
+       cout<<"palindrome"<<endl;
+       else
+       cout<<"not palindrome"<<endl;
+    return 0;
+}
