68d75ba1feb5cd09b0bd88286494204c43f58ea7
sunilmaurya <sunil.maurya@shipsy.co.in>
2021-08-27 16:40:04 +0530

    check conflict

--- string_palindrome.cpp
+++ string_palindrome.cpp
@@ -2,12 +2,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    if(s==original)
+       cout<<"palindrome"<<endl;
+       else
+       cout<<"not palindrome"<<endl;
     string s,rev;
     cout<<"enter the string"<<endl;
     cin>>s;
     string original=s;
     int n=s.length();
-    
+    if(s==original)
+       cout<<"palindrome"<<endl;
+       else
+       cout<<"not palindrome"<<endl;
+    for(int i=n-1;i<=0;i--){
+         //rev[i]=s[i];
+         cout<<s[i]<<endl;
+    }
     for(int i=n-1;i<=0;i--){
          //rev[i]=s[i];
          cout<<s[i]<<endl;
@@ -17,4 +28,8 @@ int main(){
        else
        cout<<"not palindrome"<<endl;
     return 0;
+    if(s==original)
+       cout<<"palindrome"<<endl;
+       else
+       cout<<"not palindrome"<<endl;
 }
