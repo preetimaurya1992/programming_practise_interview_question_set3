d7168c223a71000d3579a52454c4091898a478b4
Preeti Maurya <preetimaurya3344@gmail.com>
2021-08-27 13:25:56 +0530

    print string uploaded

---
+++ print_string.cpp
@@ -0,0 +1,11 @@
+//4. Write a Program to print a string.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cout<<"enter the string"<<endl;
+   cin>>s;
+   //getline(cin,s);
+    cout<<s<<endl;;
+ return 0;
+}
