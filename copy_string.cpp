34d768ca9d900b085e4cce3beb65aa6f5e178514
Preeti Maurya <preetimaurya3344@gmail.com>
2021-08-29 20:39:27 +0530

    string copy

---
+++ copy_string.cpp
@@ -0,0 +1,13 @@
+//1. Write a Program to copy a string.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1,s2;
+    cout<<"enter the strings"<<endl;
+    getline(cin,s1);
+    s2=s1;
+    cout<<"s1 :"<<s1<<endl;
+    cout<<"s2 :"<<s2<<endl;
+
+    return 0;
+}
